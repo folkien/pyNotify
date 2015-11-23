b9cbe3940aa85375c08e9c859781add29bf7374e
folkien <folkus@gmail.com>
2015-11-23 10:02:09 +0100

    Dodano force.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 echo "Instalacja pyNotify!"
 START_DIR=`pwd`
 cd /usr/bin
-sudo ln -s $START_DIR/pyNotify.py ./pyNotify
-sudo ln -s $START_DIR/pyReadNotify.py ./pyReadNotify
+sudo ln -sf $START_DIR/pyNotify.py ./pyNotify
+sudo ln -sf $START_DIR/pyReadNotify.py ./pyReadNotify
