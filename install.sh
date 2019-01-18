b851c99384f087c45ae71c03be8029fd9ec3b8e8
Sławomir Paszko <folkus@gmail.com>
2019-01-18 11:37:50 +0100

    Install fix and python version added.

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 echo "Instalacja pyNotify!"
 START_DIR=`pwd`
+sudo apt-get install gir1.2-notify-0.7
 sudo ln -sf $START_DIR/pyNotify.py          /usr/bin/pyNotify
 sudo ln -sf $START_DIR/pyReadNotify.py      /usr/bin/pyReadNotify
